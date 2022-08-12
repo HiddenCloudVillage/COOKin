34a00a791251e91ea3db287b5a63cecf3f908c65
Joseph Han <josephkhhan@gmail.com>
2022-08-12 14:37:53 -0400

    finished loading/login/landing pages

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+import { auth } from '../lib/firebase';
+
+function Header({ user }) {
+  const signOut = () => {
+    const result = confirm('signing out?');
+    if (result) {
+      auth.signOut()
+        .then(() => {
+          alert('goodbye');
+        })
+        .catch((err) => {
+          console.log(err, 'error');
+        });
+    }
+  };
+  return (
+    <HeaderContainer>
+      <Title>COOKin.</Title>
+      <HeaderRight>
+        {user
+        && <Img onClick={signOut} id="header-icon" src={user?.photoURL} alt={user?.displayName} referrerPolicy="no-referrer" />}
+      </HeaderRight>
+    </HeaderContainer>
+  );
+}
+
+export default Header;
+
+const Img = styled.img`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  &:hover {
+    cursor: pointer;
+    opacity: 70%;
+  }
+
+`;
+
+const HeaderRight = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-end;
+  margin-right: 2%;
+  width: 30%;
+`;
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  border-bottom: .5px solid black;
+  cursor: default;
+`;
+
+const Title = styled.div`
+  font-size: 35px;
+  letter-spacing: -1.5px;
+  margin-left: 2%;
+  margin-top:2%;
+  display: flex;
+  align-items: bottom;
+  &: hover{
+    opacity: 70%;
+    cursor: pointer;
+  }
+`;
+
+const Button = styled.button`
+  height: 50px;
+  width: 100px;
+  margin-top: 3%;
+  text-align: center;
+  font-size: 20px;
+  &: hover{
+    opacity: 70%;
+    cursor: pointer;
+  }
+`;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,7 @@
-
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from './components/App';
 
 const root = createRoot(document.getElementById("root"));
-
-const App = () => {
-  return <h1>Hello World</h1>
-}
-
 
 root.render(<App />);
