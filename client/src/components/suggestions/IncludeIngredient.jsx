3a30ea22b47840a35d3d99e0d1150836e241d8dc
John Economou <johnlafayeti@gmail.com>
2022-08-15 08:53:43 -0400

    include ingredient auto complete form

--- client/src/components/suggestions/IncludeIngredient.jsx
+++ client/src/components/suggestions/IncludeIngredient.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import ingredients from '../../lib/ingredients';
 
 export default function IncludeIngredient() {
+  const [inclusion, setInclusion] = useState([]);
   return (
     <div>
-      <form>
-        <input type="text" />
-      </form>
+      <Autocomplete
+        options={ingredients}
+        sx={{ width: 300 }}
+        includeInputInList
+        // eslint-disable-next-line react/jsx-props-no-spreading
+        renderInput={(params) => <TextField {...params} label="Include" />}
+        // eslint-disable-next-line max-len
+        onInputChange={(e, newInputValue) => setInclusion((oldArray) => [...oldArray, newInputValue])}
+      />
+      <div>
+        Include Ingredients:
+        {[...new Set(inclusion)].map((ingredient) => <div>{ingredient}</div>)}
+      </div>
     </div>
   );
 }
