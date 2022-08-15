2773e4dd4a088dd71482217fc28d46f725fa89a8
John Economou <johnlafayeti@gmail.com>
2022-08-15 12:22:10 -0400

    styling include

--- client/src/components/suggestions/IncludeIngredient.jsx
+++ client/src/components/suggestions/IncludeIngredient.jsx
@@ -1,4 +1,5 @@
 /* eslint-disable react/jsx-props-no-multi-spaces */
+import styled from 'styled-components';
 import React from 'react';
 import { Autocomplete, TextField } from '@mui/material/';
 import ingredients from '../../lib/ingredients';
@@ -11,25 +12,67 @@ export default function IncludeIngredient({ inclusion, setInclusion }) {
     setInclusion(newInclusion);
   }
   return (
-    <div>
-      <Autocomplete
-        options={ingredients}
-        sx={{ width: 300 }}
-        includeInputInList
-        // eslint-disable-next-line react/jsx-props-no-spreading
-        renderInput={(params) => <TextField {...params} label="Include" />}
-        // eslint-disable-next-line max-len, no-unused-expressions
-        onChange={(e, newInputValue) => { newInputValue === null ? null : setInclusion((oldArray) => [...oldArray, newInputValue]); }}
-      />
-      <div>
-        Include Ingredients:
-        {[...new Set(inclusion)].map((ingredient) => (
-          <div key={ingredient}>
-            {ingredient}
-            <button type="submit" onClick={(e) => handleClick(e, ingredient)}> X </button>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container>
+      <Title> Include </Title>
+      <InnerContainer>
+        <Autocomplete
+          options={ingredients}
+          sx={{ width: 300 }}
+          includeInputInList
+          // eslint-disable-next-line react/jsx-props-no-spreading
+          renderInput={(params) => <TextField {...params} />}
+          // eslint-disable-next-line max-len, no-unused-expressions
+          onChange={(e, newInputValue) => { newInputValue === null ? null : setInclusion((oldArray) => [...oldArray, newInputValue]); }}
+        />
+        <List>
+          {[...new Set(inclusion)].map((ingredient) => (
+            <Row key={ingredient}>
+              {ingredient}
+              <button type="submit" onClick={(e) => handleClick(e, ingredient)}> X </button>
+            </Row>
+          ))}
+        </List>
+      </InnerContainer>
+    </Container>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 350px;
+  height: 500px;
+  background-color: #D4A373;
+`;
+
+const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #FAEDCD;
+  width: 290px;
+`;
+
+const InnerContainer = styled.div`
+  margin-top: 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #FAEDCD;
+  height: 390px;
+`;
+
+const List = styled.div`
+  margin-top: 20px;
+  margin-bottom: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #FAEDCD;
+`;
+
+const Title = styled.h1`
+color: #FAEDCD;
+`;
