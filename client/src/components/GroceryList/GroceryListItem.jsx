1d6b124e1a5b43c3dab16786c9da21083b38ec72
jaredshedr <jaredsurfside@gmail.com>
2022-08-15 09:53:31 -0400

    pantry changes in gortcery list

--- client/src/components/GroceryList/GroceryListItem.jsx
+++ client/src/components/GroceryList/GroceryListItem.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 
 function GroceryListItem({ ingredient, pantry }) {
-  console.log(ingredient, pantry);
+  let pantryArray = Object.keys(pantry);
+  console.log(ingredient, pantryArray);
   return (
     <div>
       <span>{ingredient}</span>
