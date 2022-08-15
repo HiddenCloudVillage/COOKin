2536df64c8838336ec5087b11cd262329e7ef673
John Economou <johnlafayeti@gmail.com>
2022-08-15 19:50:09 -0400

    work

--- client/src/components/suggestions/Suggestions.jsx
+++ client/src/components/suggestions/Suggestions.jsx
@@ -5,7 +5,6 @@ import ExcludeIngredient from './ExcludeIngredient';
 
 export default function Suggestions({ recipes, userInfo, setUserInfo,
 }) {
-export default function Suggestions({ recipes, userInfo, setUserInfo }) {
   const [includeIngredients, setIncludeIngredients] = useState([]);
   const [excludeIngredients, setExcludeIngredients] = useState([]);
 
@@ -25,8 +24,11 @@ export default function Suggestions({ recipes, userInfo, setUserInfo }) {
           include = false;
         }
       });
+
       return include && !exclude;
     });
+    console.log(recipes);
+    console.log('filteredRecipes', filteredRecipes);
 
     const pantryIngredients = userInfo.pantry ? Object.keys(userInfo?.pantry) : [];
     const recipesWithPercent = filteredRecipes.map((recipe) => {
@@ -42,24 +44,6 @@ export default function Suggestions({ recipes, userInfo, setUserInfo }) {
     }).sort((a, b) => b.percent - a.percent);
     return recipesWithPercent;
   };
-    if (userInfo.pantry !== undefined) {
-      const pantryIngredients = Object.keys(userInfo?.pantry);
-      const recipesWithPercent = filteredRecipes.map((recipe) => {
-        const recipeIngredients = recipe.ingredients.map((ingredient) => ingredient.ingredientName);
-        const recipePercent = recipeIngredients.reduce((acc, ingredient) => {
-          if (pantryIngredients.includes(ingredient)) {
-            return acc + 1;
-          }
-          return acc;
-        }, 0);
-        const percent = Math.floor((recipePercent / recipeIngredients.length) * 100);
-        return { ...recipe, percent };
-      }).sort((a, b) => b.percent - a.percent);
-      // setFilteredAndSortedRecipes(recipesWithPercent);
-      return recipesWithPercent;
-    }
-  };
-
   return (
     <div>
       <div>
