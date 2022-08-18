67248991622fdcad0b5d171187bf9cb17a396f22
Ivan Moreno <Ivandmoreno1985@gmail.com>
2022-08-18 09:35:11 -0400

    added markers to nearest store

--- client/src/components/GroceryList/GroceryStore.jsx
+++ client/src/components/GroceryList/GroceryStore.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 import mapKey from '../../lib/mapKey';
+
+const axios = require('axios');
 
 function GroceryStore() {
   const [center, setCenter] = useState('');
+  const [stores, setStores] = useState();
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -11,25 +14,44 @@ function GroceryStore() {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.coords.latitude}%2C${position.coords.longitude}&radius=1500&type=store&keyword=grocery&key=${mapKey}`;
+      axios({
+        method: 'put',
+        url: '/getStores',
+        data: { url },
+      })
+        .then((res) => {
+          const storeArray = res.data.results.map(
+            (store) => store.geometry.location,
+          );
+          setStores(storeArray);
+        })
+        .catch((err) => console.log('🚨', err));
     });
   }, []);
 
   const options = {
-    mapId: '268e33d195d16b40',
+    mapId: 'e0dcd433510d5496',
   };
   const mapStyles = {
     height: '50vh',
     width: '50%',
   };
-
+  if (!stores) {
+    return null;
+  }
   return (
     <LoadScript googleMapsApiKey={mapKey}>
       <GoogleMap
         mapContainerStyle={mapStyles}
-        zoom={15}
+        zoom={16}
         center={center}
         options={options}
-      />
+      >
+        {stores.map((eachStore) => (
+          <Marker position={eachStore} />
+        ))}
+      </GoogleMap>
     </LoadScript>
   );
 }
