d251e8e1ee4a55c4d15d2e7dc9f44ac1971bc2da
Ivan Moreno <Ivandmoreno1985@gmail.com>
2022-08-18 15:48:54 -0400

    updated store search url

--- client/src/components/GroceryList/GroceryStore.jsx
+++ client/src/components/GroceryList/GroceryStore.jsx
@@ -15,7 +15,7 @@ function GroceryStore() {
         lat: position.coords.latitude,
         lng: position.coords.longitude,
       });
-      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.coords.latitude}%2C${position.coords.longitude}&radius=1500&type=store&keyword=grocery&key=${mapKey}`;
+      const url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${position.coords.latitude}%2C${position.coords.longitude}&radius=500&type=grocery_or_supermarket&key=${mapKey}`;
       axios({
         method: 'put',
         url: '/getStores',
